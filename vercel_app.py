84b75ecca239eba5db186e75fa46650b08b97d26
roseraph502on <sera6768@naver.com>
2026-05-26 20:17:32 +0900

    vercel파일 수정

--- vercel_app.py
+++ vercel_app.py
@@ -1,13 +1,25 @@
-# vercel_app.py
+# import os
+# import sys
+# from django.core.wsgi import get_wsgi_application
+
+# # 1. Vercel 서버 환경에 현재 최상위 경로를 강제로 인식시킴
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# # 2. 장고 환경설정 연결
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# # 3. WSGI 애플리케이션 생성
+# app = get_wsgi_application()
 import os
 import sys
-from django.core.wsgi import get_wsgi_application
 
-# 1. Vercel 서버 환경에 현재 최상위 경로를 강제로 인식시킴
+# 현재 프로젝트 디렉토리를 파이썬 경로에 명시적으로 추가
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-# 2. 장고 환경설정 연결
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+# 장고 환경 설정을 config.settings로 지정
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-# 3. WSGI 애플리케이션 생성
+from django.core.wsgi import get_wsgi_application
+
+# Vercel이 인식할 WSGI 핸들러 'app' 선언
 app = get_wsgi_application()
