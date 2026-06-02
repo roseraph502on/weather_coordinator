f4ccb24d667d4738de2a3cb394fd958e08ddbf53
roseraph502on <sera6768@naver.com>
2026-06-02 16:17:42 +0900

    배포 오류

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,16 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# dirname을 두 번 써서 config 폴더가 아닌 '프로젝트 최상위 루트' 경로를 추출
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# 파이썬 검색 경로에 최상위 루트 디렉토리를 명시적으로 추가
+sys.path.append(BASE_DIR)
+
+# 장고 환경 설정을 config.settings로 지정
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
+# Vercel 및 Django runserver가 이 'application'을 정상적으로 호출합니다.
 application = get_wsgi_application()
