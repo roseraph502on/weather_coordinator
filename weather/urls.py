baa7093ecc3ebb2e8435f932a22fc20885ebb32f
roseraph502on <sera6768@naver.com>
2026-06-02 15:46:09 +0900

    파피콘

--- weather/urls.py
+++ weather/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('get-outfits/', views.get_outfits, name='get_outfits'), # 이미지칸 전용 통로 
+    path('favicon.ico', lambda request: HttpResponse(status=204)),
 ]
