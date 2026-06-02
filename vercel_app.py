e2bf31f4400706d61366583d7ef3715c95c954ef
roseraph502on <sera6768@naver.com>
2026-06-02 16:22:33 +0900

    배포 오류 22

--- vercel_app.py
+++ vercel_app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-# 🎯 [수정 핵심] dirname을 두 번 써서 config 폴더가 아닌 '프로젝트 최상위 루트' 경로를 추출합니다.
+# dirname을 두 번 써서 config 폴더가 아닌 '프로젝트 최상위 루트' 경로를 추출합니다.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # 파이썬 검색 경로에 최상위 루트 디렉토리를 명시적으로 추가
@@ -10,7 +10,6 @@ sys.path.append(BASE_DIR)
 # 장고 환경 설정을 config.settings로 지정
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-from django.core.wsgi import get_wsgi_application
-
-# Vercel이 인식할 WSGI 핸들러 'app' 선언
-app = get_wsgi_application()
+# config.wsgi의 표준 WSGI callable을 그대로 가져옵니다.
+from config.wsgi import application
+app = application
