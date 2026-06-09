98d0deb31369b4816575631bdf2cc7a731d8890a
roseraph502on <sera6768@naver.com>
2026-06-09 19:24:18 +0900

    ㅠ

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 # 5. 미들웨어 설정
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -92,11 +93,12 @@ USE_I18N = True
 USE_TZ = True
 
 # 10. 정적 파일 경로
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # 11. 기본 자동 필드 타입
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
